b6f6328a6bfa30b6dad3d40f1cb34b80bdc8a0f8
ethandamian <ethandamian@ciencias.unam.mx>
2023-10-11 22:34:52 -0600

    Tablas Notificar,Vet,Alimento,Proveedor,Cuidador,Trabajar

--- SQL/DDL.sql
+++ SQL/DDL.sql
@@ -31,21 +31,121 @@ CREATE TABLE CorreoProveedor(
 
 CREATE TABLE ProveerMedicina(
 	IDInsumoMedicina serial NOT NULL CHECK(IDInsumoMedicina > 0),
+<<<<<<< Updated upstream
 	RFCProveedor VARCHAR(13) NOT NULL CHECK(LENGTH(RFCProveedor) = 13 OR LENGTH(RFCProveedor) = 12 AND RFCProveedor LIKE '^[A-Z]{4}[0-9]{6}[A-Z0-9]{0,3}$')
+=======
+	RFCProveedor VARCHAR(13) NOT NULL CHECK(RFCProveedor <> '')
+>>>>>>> Stashed changes
 );
 
 --------TABLAS CON LLAVES PRIMARIAS-----------------
 CREATE TABLE CorreoVeterinario(
-	RFCVeterinario VARCHAR(13) CHECK(LENGTH(RFCVeterinario) = 13 OR LENGTH(RFCVeterinario) = 12
-									 AND RFCVeterinario <> ''),
+	RFCVeterinario VARCHAR(13) CHECK(RFCVeterinario <> '' AND (LENGTH(RFCVeterinario = 13) OR LENGTH(RFCVeterinario = 12))),
 	Correo VARCHAR(50)  CHECK (Correo LIKE '%_@_%._%' AND Correo <> ''),
 	PRIMARY KEY(RFCVeterinario,Correo)
 );
 
 CREATE TABLE TelefonoVeterinario(
-	RFCVeterinario VARCHAR(13) CHECK(LENGTH(RFCVeterinario) = 13 OR LENGTH(RFCVeterinario) = 12 
-									 AND RFCVeterinario <> ''),
+	RFCVeterinario VARCHAR(13) CHECK(RFCVeterinario <> '' AND (LENGTH(RFCVeterinario = 13) OR LENGTH(RFCVeterinario = 12) )),
 	Telefono CHAR(10) CHECK(Telefono ~ '^[0-9 ]*$' AND Telefono <> ''),
 	PRIMARY KEY(RFCVeterinario,Telefono)
 
 );
+
+------------------------------PARTE ETHAN -----------------------------------------------
+CREATE TABLE Notificar(
+	idEvento INT NOT NULL,
+	IDVisitante INT NOT NULL,
+	TipoNotificacion VARCHAR(50) NOT NULL CHECK (TipoNotificacion <> '')
+	FOREIGN KEY (idEvento) REFERENCES Evento(idEvento),
+	FOREIGN KEY (IDVisitante) REFERENCES Visitante(IDVisitante)
+
+);
+
+CREATE TABLE Veterinario(
+	RFCVeterinario VARCHAR(13) CHECK (RFCVeterinario <> '', AND (LENGTH(RFCVeterinario = 13) OR LENGTH(RFCVeterinario = 12) )),
+	Nombre VARCHAR(50) NOT NULL CHECK(Nombre <> '' AND Nombre LIKE '%[a-zA-Z]%'),
+	ApellidoPaterno VARCHAR(50) NOT NULL CHECK(ApellidoPaterno <> '' AND ApellidoPaterno LIKE '%[a-zA-Z]%'),
+	ApellidoMaterno VARCHAR(50) NOT NULL CHECK(ApellidoMaterno <> '' AND ApellidoMaterno LIKE '%[a-zA-Z]%'),
+	Calle VARCHAR(50) NOT NULL CHECK(Calle <> ''),
+	NumInterior INT NOT NULL,
+	NumExterior INT NOT NULL,
+	Colonia VARCHAR(50) NOT NULL CHECK(Colonia <> ''),
+	Estado VARCHAR(50) NOT NULL CHECK(Estado <> '' AND Estado LIKE '%[a-zA-Z]%'),
+	FechaInicioContrato DATE NOT NULL,
+	FechaFinContrato DATE NOT NULL,
+	FechaNacimiento DATE NOT NULL,
+	Genero VARCHAR(10) NOT NULL CHECK(Genero <> '' AND Genero LIKE '%[a-zA-Z]%'),
+	Salario DECIMAL NOT NULL CHECK(Salario > 0),
+	Especialidad VARCHAR(50) NOT NULL CHECK(Especialidad <> '' AND Especialidad LIKE '%[a-zA-Z]%')
+	PRIMARY KEY(RFCVeterinario)
+	
+);
+
+
+CREATE TABLE Proveedor(
+	RFCProveedor VARCHAR(13) CHECK (RFCProveedor <> '',AND (LENGTH(RFCProveedor = 13) OR LENGTH(RFCProveedor = 12) )),
+	Nombre VARCHAR(50) NOT NULL CHECK(Nombre <> '' AND Nombre LIKE '%[a-zA-Z]%'),
+	ApellidoPaterno VARCHAR(50) NOT NULL CHECK(ApellidoPaterno <> '' AND ApellidoPaterno LIKE '%[a-zA-Z]%'),
+	ApellidoMaterno VARCHAR(50) NOT NULL CHECK(ApellidoMaterno <> '' AND ApellidoMaterno LIKE '%[a-zA-Z]%'),
+	Calle VARCHAR(50) NOT NULL CHECK(Calle <> ''),
+	NumInterior INT NOT NULL,
+	NumExterior INT NOT NULL,
+	Colonia VARCHAR(50) NOT NULL CHECK(Colonia <> ''),
+	Estado VARCHAR(50) NOT NULL CHECK(Estado <> '' AND Estado LIKE '%[a-zA-Z]%'),
+	FechaInicioContrato DATE NOT NULL,
+	FechaFinContrato DATE NOT NULL,
+	FechaNacimiento DATE NOT NULL,
+	Genero VARCHAR(10) NOT NULL CHECK(Genero <> '' AND Genero LIKE '%[a-zA-Z]%'),
+	FrecuenciaServicio INT NOT NULL CHECK(FrecuenciaServicio > 0),
+	CostoServicio INT NOT NULL CHECK(CostoServicio > 0),
+	PRIMARY KEY(RFCProveedor)
+	
+);
+
+
+CREATE TABLE Cuidador(
+	RFCCuidador VARCHAR(13) CHECK (RFCCuidador <> '',AND (LENGTH(RFCCuidador = 13) OR LENGTH(RFCCuidador = 12) )),
+	IDBioma INT NOT NULL, 
+	Nombre VARCHAR(50) NOT NULL CHECK(Nombre <> '' AND Nombre LIKE '%[a-zA-Z]%'),
+	ApellidoPaterno VARCHAR(50) CHECK(ApellidoPaterno <> '' AND ApellidoPaterno LIKE '%[a-zA-Z]%'),
+	ApellidoMaterno VARCHAR(50) CHECK(ApellidoMaterno <> '' AND ApellidoMaterno LIKE '%[a-zA-Z]%'),
+
+	CONSTRAINT chk_apellidos CHECK (
+        (ApellidoPaterno IS NOT NULL OR ApellidoMaterno IS NOT NULL)
+    ),
+	Calle VARCHAR(50) NOT NULL CHECK(Calle <> ''),
+	NumInterior INT NOT NULL,
+	NumExterior INT NOT NULL,
+	Colonia VARCHAR(50) NOT NULL CHECK(Colonia <> ''),
+	Estado VARCHAR(50) NOT NULL CHECK(Estado <> '' AND Estado LIKE '%[a-zA-Z]%'),
+	FechaInicioContrato DATE NOT NULL,
+	FechaFinContrato DATE NOT NULL,
+	FechaNacimiento DATE NOT NULL,
+	Genero VARCHAR(10) NOT NULL CHECK(Genero <> '' AND Genero LIKE '%[a-zA-Z]%'),
+	DiasTrabajo INT NOT NULL CHECK(DiasTrabajo > 0),
+	HorarioLaboral VARCHAR(50) NOT NULL CHECK(HorarioLaboral <> ''),
+	Salario DECIMAL NOT NULL  CHECK(Salario > 0),
+	PRIMARY KEY(RFCCuidador),
+	FOREIGN KEY (IDBioma) REFERENCES Bioma(IDBioma)
+	
+);
+
+CREATE TABLE Alimento(
+	IDInsumoAlimento INT,
+	Nombre VARCHAR(50) NOT NULL CHECK (Nombre <> '' AND Nombre LIKE '%[a-zA-Z]%'),
+	Cantidad INT NOT NULL CHECK(Cantidad > 0),
+	FechaCaducidad DATE NOT NULL CHECK(FechaCaducidad < CURRENT_DATE),
+	Refrigeracion BOOL NOT NULL,
+	TipoAlimento VARCHAR(50) NOT NULL CHECK(TipoAlimento <> ''),
+	PRIMARY KEY(IDInsumoAlimento)
+		
+);
+
+CREATE TABLE Trabajar(
+	RFCVeterinario VARCHAR(13) NOT NULL,
+	IDBioma INT NOT NULL,
+	FOREIGN KEY (RFCVeterinario) REFERENCES Veterinario(RFCVeterinario),
+	FOREIGN KEY (IDBioma) REFERENCES Bioma(IDBioma)
+);
+--------------- FINAL PARTE ETHAN ---------------------------------
